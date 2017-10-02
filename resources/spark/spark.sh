db27446652ebd86314ff3904df51e3473d50358b
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-02 01:20:30 +0100

    Made Hadoop default libraries available for Apache Spark installation

--- resources/spark/spark.sh
+++ resources/spark/spark.sh
@@ -2,3 +2,12 @@
 
 export SPARK_HOME=/usr/local/spark
 export PATH=${SPARK_HOME}/bin:${SPARK_HOME}/sbin:${PATH}
+
+# Add hadoop native library
+
+if [ -z "$var" ]
+then
+	export LD_LIBRARY_PATH=${HADOOP_YARN_HOME}/lib/native/
+else
+	export LD_LIBRARY_PATH=${HADOOP_YARN_HOME}/lib/native/:${LD_LIBRARY_PATH}
+fi
