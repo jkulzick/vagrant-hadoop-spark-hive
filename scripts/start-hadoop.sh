1fb62ed42b8a9de1230eda5b31a038472160ca13
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-18 21:29:10 +0100

    starting hadoop with hadoop 3 conventions

--- scripts/start-hadoop.sh
+++ scripts/start-hadoop.sh
@@ -4,12 +4,12 @@ source "/vagrant/scripts/common.sh"
 
 function startDaemons {
     echo "starting Hadoop daemons"
-    $HADOOP_PREFIX/sbin/hadoop-daemon.sh --config $HADOOP_CONF_DIR --script hdfs start namenode
-    $HADOOP_PREFIX/sbin/hadoop-daemons.sh --config $HADOOP_CONF_DIR --script hdfs start datanode
-    $HADOOP_YARN_HOME/sbin/yarn-daemon.sh --config $HADOOP_CONF_DIR start resourcemanager
-    $HADOOP_YARN_HOME/sbin/yarn-daemons.sh --config $HADOOP_CONF_DIR start nodemanager
-    $HADOOP_YARN_HOME/sbin/yarn-daemon.sh start proxyserver --config $HADOOP_CONF_DIR
-    $HADOOP_PREFIX/sbin/mr-jobhistory-daemon.sh start historyserver --config $HADOOP_CONF_DIR
+    hdfs --config $HADOOP_CONF_DIR --daemon start namenode
+    hdfs --config $HADOOP_CONF_DIR --daemon start datanode
+    yarn --config $HADOOP_CONF_DIR --daemon start resourcemanager
+    yarn --config $HADOOP_CONF_DIR --daemon start nodemanager
+    yarn --config $HADOOP_CONF_DIR --daemon start proxyserver 
+    yarn --config $HADOOP_CONF_DIR --daemon start historyserver
 
     echo "waiting for HDFS to come up"
     # loop until at least HDFS is up
