2ea263b3f2f393add1c0680bb25b5581cdb39bcc
Aaron Tello-Wharton <aaron.tkd@hotmail.com>
2017-10-21 13:49:02 +0100

    now hdfs is on external drive

--- scripts/mount.sh
+++ scripts/mount.sh
@@ -5,3 +5,7 @@ yum -y install ntfs-3g
 
 mkdir -p /mnt/big_boi
 sudo mount -t ntfs /dev/sdb2 /mnt/big_boi
+
+mkdir -p /mnt/big_boi/hadoop_data
+mkdir -p /mnt/big_boi/hadoop_data/hadoop-namenode
+mkdir -p /mnt/big_boi/hadoop_data/hadoop-datanode
