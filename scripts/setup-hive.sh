cc3f64afedcde2598691799a9eaee82dd9ebad13
johnkulzick <jkulzick@civisanalytics.com>
2018-06-21 14:30:39 -0500

    ENH simplify installHive and comment out wget of mysql jar

--- scripts/setup-hive.sh
+++ scripts/setup-hive.sh
@@ -1,28 +1,20 @@
 #!/bin/bash
 
+set -e -x
+
 # http://www.cloudera.com/content/cloudera/en/documentation/cdh4/v4-2-0/CDH4-Installation-Guide/cdh4ig_topic_18_4.html
 
 source "/vagrant/scripts/common.sh"
 
-function installLocalHive {
-	echo "install hive from local file"
-	FILE=/vagrant/resources/$HIVE_ARCHIVE
-	tar -xzf $FILE -C /usr/local
-}
-
-function installRemoteHive {
-	echo "install hive from remote file"
-	curl -sS -o /vagrant/resources/$HIVE_ARCHIVE -O -L $HIVE_MIRROR_DOWNLOAD
-	tar -xzf /vagrant/resources/$HIVE_ARCHIVE -C /usr/local
-}
-
 function installHive {
-	if resourceExists $HIVE_ARCHIVE; then
-		installLocalHive
-	else
-		installRemoteHive
+	if ! resourceExists $HIVE_ARCHIVE; then
+	    echo "downloading $HIVE_ARCHIVE"
+	    curl -sS -o /vagrant/resources/$HIVE_ARCHIVE -O -L $HIVE_MIRROR_DOWNLOAD
 	fi
-	ln -s /usr/local/apache-$HIVE_VERSION-bin /usr/local/hive
+
+    echo "installing hive"
+	tar -xzf /vagrant/resources/$HIVE_ARCHIVE -C /usr/local
+	ln -s /usr/local/$HIVE_ARCHIVE_PREFIX /usr/local/hive
 	mkdir /usr/local/hive/logs /usr/local/hive/derby/
 }
 
@@ -38,8 +30,8 @@ function setupEnvVars {
 }
 
 function initSchema {
-	wget http://central.maven.org/maven2/mysql/mysql-connector-java/8.0.7-dmr/mysql-connector-java-8.0.7-dmr.jar
-	mv mysql-connector-java-8.0.7-dmr.jar $HIVE_HOME/lib/
+	#wget http://central.maven.org/maven2/mysql/mysql-connector-java/8.0.7-dmr/mysql-connector-java-8.0.7-dmr.jar
+	#mv mysql-connector-java-8.0.7-dmr.jar $HIVE_HOME/lib/
 	$HIVE_HOME/bin/schematool -dbType mysql -initSchema
 }
 
